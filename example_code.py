62c5eddc3e6ff1a02165532598b040c2e942c917
Liam Kenneally <liamkenneally@msn.com>
2021-12-27 02:16:08 +0000

    fixed class naming errors after refactoring 'Sites' class to 'Organization' - example_code.py still referenced 'sites' in the code example.

--- example_code.py
+++ example_code.py
@@ -5,27 +5,27 @@ from mist import *
 dashboard = DashboardAPI(api_key=config.API_KEY_ENVIRONMENT_VARIABLE)
 
 
-# Call the getSelf() method from the Sites Class 
+# Call the getSelf() method from the Organization Class 
 # Extract the Mist org_id for future use in the script
-mist_self = dashboard.sites.getSelf()
+mist_self = dashboard.organization.getSelf()
 org_id = mist_self["privileges"][0]["org_id"]
 
 
-# Call the getSites() method from the Sites Class
+# Call the getSites() method from the Organization Class
 # Extract the required Mist site_id for future use in the script
-mist_site = dashboard.sites.getSites(org_id)
+mist_site = dashboard.organization.getSites(org_id)
 site_id = mist_site[0]["id"]
 
 
 # get RFTemplate & NetworkTemplate for site creation later in script
-rftemp = dashboard.sites.getRFTemplates(org_id)
+rftemp = dashboard.organization.getRFTemplates(org_id)
 rftemplate_id = rftemp[0]["id"]
-nwtemp = dashboard.sites.getNetworkTemplates(org_id)
+nwtemp = dashboard.organization.getNetworkTemplates(org_id)
 networktemplate_id = nwtemp[0]["id"]
 
 
 # Create a Mist Site
-create_examplesite = dashboard.sites.createSite(org_id, 
+create_examplesite = dashboard.organization.createSite(org_id, 
                                                 "Test Site by API", 
                                                 "Created via Python API - final test", 
                                                 "Europe/London",
